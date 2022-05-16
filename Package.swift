f3d9e2f37af8b9df290d9ac2d7f748c18799b805
Garnik Ghazaryan <garnik_ghazaryan@epam.com>
2022-05-16 13:38:53 +0400

    Changed package file

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["AppCustomization"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/GPretadoR/Development-Support.git", from: "1.0.1")
+        .package(url: "https://github.com/GPretadoR/Development-Support.git", branch: "master")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
